68c0bf6115ba2065c3bfcb287fb13e2fc10f6b1c
Lourenço Lima da Cunha <lourenco.cunha@POA.E-CORE.COM>
2017-08-17 14:26:21 -0300

    aula 01 e 02

---
+++ alurapic/client/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core"
+
+@Component({
+    moduleId : module.id,
+    templateUrl : "./app.component.html",
+    selector : "app"
+})
+export class AppComponent {
+
+}
---
+++ alurapic/client/app/foto/foto.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    moduleId : module.id,
+    templateUrl : "./foto.component.html",
+    selector : "foto"
+})
+export class FotoComponent {
+
+    @Input() titulo;
+    @Input() url;
+
+}
